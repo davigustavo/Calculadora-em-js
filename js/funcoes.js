7889764140b44dc5e0e985c6b87fb564b08fe34e
Davi Gustavo <davigustavo16@outlook.com>
2020-12-09 21:39:11 -0300

    Finalizando a Calculadora

--- js/funcoes.js
+++ js/funcoes.js
@@ -44,3 +44,20 @@ function InserirMult() {
 function InserirPonto() {
     document.getElementById("visor").value += ".";
 }
+function LimparVisor() {
+    document.getElementById("visor").value = "";
+}
+function RaizQuadrada() {
+    let numero = document.getElementById("visor").value;
+    document.getElementById("visor").value = Math.sqrt(numero); 
+}
+function Calcular() {
+    let expressao = document.getElementById("visor").value;
+    let resultado = eval(expressao);
+    document.getElementById("visor").value = resultado;
+}
+
+function Apagar() {
+    let conteudo = document.getElementById("visor").value;
+    document.getElementById("visor").value = conteudo.substring(0,conteudo.length - 1);
+}
